8740572e9f058a83441d23d5080ed85c00fa53f2
imohammadali <ali1995ez@gmail.com>
2024-05-26 20:01:11 +0330

    chore: add token interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-interceptors';
+  constructor(private _http: HttpClient) {
+    localStorage.setItem('token','auth-token-12345')
+    this._http.get('http://localhost:3000/users').subscribe();
+  }
+
 }
